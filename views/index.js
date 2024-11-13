38e4884217dd6ac339c3c1d32e7ee4770051ab20
jeff <devil1032@gmail.com>
2024-11-13 14:34:30 +0900

    review: user-controller.js

--- views/index.js
+++ views/index.js
@@ -13,7 +13,11 @@ import dotenv from 'dotenv'
 dotenv.config({ path: '../.env' })
 const app = express()
 const PORT = 3000
-const secret_key = process.env.SECRET_KEY
+
+// [jeff] 환경변수로 키를 넘겨줘야 하는 경우 package.json 내 script 키 값 안에 포함시키거나 전용 실행 스크립트를 작성하는게 좋습니다.
+// 실제 프로젝트에서 환경변수를 어디까지 노출시켜야 하는지에 대해서는 상황마다 방법이 다 다릅니다. 깃헙 시크릿도 있고 AWS Secret manager 도 있고 별도 외부 저장소에서 끌어오는 방법 등등..
+// 지금은 테스트를 위해 하드코딩 진행하겠습니다.
+const secret_key = "123123"
 
 app.use(
   cors({
